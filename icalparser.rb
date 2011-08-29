326bdd835345cf49696e92b55c3e47b7be3ad074
Matthieu Riviere <poldenton@leukensis.org>
2011-08-29 01:09:51 +0000

    Add support for simple filters.
    For now, only print the events on the next 7 days

--- icalparser.rb
+++ icalparser.rb
@@ -28,17 +28,17 @@ class Course
 end
 
 # Updates the courses hash
-def updateCourses
+def updateCourses(&block)
 	puts 'Fetching file...'
 	uri = URI.parse(@ical_url)
 	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
 	res = http.request_get(uri.request_uri)
 
-	parseFile res.body
+	parseFile(res.body, &block)
 end
 
-def parseFile(cal_string)
+def parseFile(cal_string, &block)
 	puts 'Parsing file...'
 
 	cals = Icalendar.parse(cal_string)
@@ -52,12 +52,14 @@ def parseFile(cal_string)
 	@courses = Hash.new
 
 	evts.each { |evt| 
-		key = canonicalDate evt.dtstart
-		course = Course.new(evt.dtstart, evt.dtend, evt.summary, evt.description, evt.location)
-		if @courses[key] == nil then
-			@courses[key] = [course]
-		else
-			@courses[key][@courses[key].length] = course
+		if ((block_given? && (yield evt) == true) || !block_given?) then
+			key = canonicalDate evt.dtstart
+			course = Course.new(evt.dtstart, evt.dtend, evt.summary, evt.description, evt.location)
+			if @courses[key] == nil then
+				@courses[key] = [course]
+			else
+				@courses[key][@courses[key].length] = course
+			end
 		end
 
 	}
@@ -78,6 +80,18 @@ def printCourses(courses_list)
 	}
 end
 
-updateCourses
+def thisWeek(evt)
+	def t(d)
+		now = Date.today - 1
+		inaweek = Date.today + 7
+		if d >= now and d <= inaweek then true else false end
+	end
+	b = (t evt.start) || (t evt.end)
+	return b
+end
+
+#updateCourses
+updateCourses { |evt| thisWeek(evt) }
 
 printCourses(@courses)
+
